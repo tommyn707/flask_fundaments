c99f7cddfe70e7bfaf5b41f76dfbcbabb9f1c2aa
tommyn707 <tommy.n707@gmail.com>
2020-02-06 13:53:15 -0800

    first commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/dojo')
+def dojo():
+  return "Dojo!"
+
+@app.route('/say/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def say(name):
+    print(name)
+    return f"Hi, {name}"
+@app.route("/repeat/<number>/<word>")
+def repeat(number, word):
+    print(number, word)
+    return word *int(number)
+  
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
+
+    
